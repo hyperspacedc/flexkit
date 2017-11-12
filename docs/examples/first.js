25fb32e4b2b54a4e2ed8ca7247db9793b7ab1c04
Alex Freska <alex.freska@gmail.com>
2017-11-12 16:32:11 -0500

    Export vars

--- docs/examples/first.js
+++ docs/examples/first.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import { View, Flex } from '../../src/flex'
+import { View, Flex } from '../../src'
 import { Code } from '../entry'
 
 const Sidebar = styled.div`
